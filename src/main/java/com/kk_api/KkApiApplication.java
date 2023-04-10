78a3c4df5d64771d178ceece00798a6540d08223
Lee <1019918995@qq.com>
2023-04-10 20:06:20 +0800

    2023.4.10

---
+++ src/main/java/com/kk_api/KkApiApplication.java
@@ -0,0 +1,13 @@
+package com.kk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kk_api/KkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
